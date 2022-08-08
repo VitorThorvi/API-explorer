05d8e2c137d24005c7bc2897e26ac56c742bae47
Vitor Thorvi <euvitoreu@gmail.com>
2022-08-08 10:12:12 -0300

    reimplementation

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 require("express-async-errors");
 const migrationsRun = require("./database/sqlite/migrations");
-const AppError = require("./utils/AppError");
 
+const AppError = require("./utils/AppError");
 const express = require('express');
 const routes = require("./routes")
-
 migrationsRun();
 
 const app = express();
@@ -14,13 +13,10 @@ app.use(routes);
 
 app.use((error, request, response, next) => {
     if (error instanceof AppError) {
-        return response.status(error.statusCode).json({
-            status: "error",
-            message: error.message
-        });
+        return response.status(error.statusCode).json({status: "error", message: error.message})
     }
 
-    console.error(error);
+    console.error(error)
 
     return response.status(500).json({
         status: "error",
